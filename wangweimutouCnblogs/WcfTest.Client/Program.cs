e25ef57fee351a831c98e8d5581dd31a05d50cd6
blackheart100 <1290823140@qq.com>
2018-01-24 15:47:21 +0800

    ..

---
+++ wangweimutouCnblogs/WcfTest.Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WcfTest.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1Client proxy = new Service1Client();
+            string strMsg = proxy.GetData(12);
+            Console.WriteLine(strMsg);
+            Console.Read();
+        }
+    }
+}
